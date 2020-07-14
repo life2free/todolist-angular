1b8f76875f7ec984feba71701216e200a2a68853
shimin <thunderous_0518@hotmail.com>
2020-07-14 17:27:33 -0400

    populate the data to todo component and add css

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-completed': this.todo.completed,
+    };
+    return classes;
   }
 
+  onToggle(todo: Todo) {
+    this.todo.completed = !this.todo.completed;
+  }
+
+  onDelete(todo: Todo) {}
 }
